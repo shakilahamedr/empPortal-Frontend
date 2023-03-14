1a2a7c8f9c1d810a4b64f6f764b9946f76d5ee26
shakil <shakilahamed0607@gmail.com>
2023-03-14 12:53:57 +0530

    saving empId in local storage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,58 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { UpdateComponent } from './components/update/update.component';
+import { AuthGuard } from './services/auth.guard';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  {
+    path: "",
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: "login",
+    component: LoginComponent,
+    pathMatch: "full"
+  },
+  {
+    path: "register",
+    component: RegisterComponent,
+    pathMatch: 'full'
+  },
+  // {
+  //   path: "dashboard/:id",
+  //   component: DashboardComponent,
+  //   pathMatch: 'full',
+  //   canActivate: [AuthGuard]
+  // },
+  // {
+  //   path: "update/:id",
+  //   component: UpdateComponent,
+  //   pathMatch: 'full',
+  //   canActivate: [AuthGuard]
+  // },
+
+  //
+  {
+    path: "dashboard",
+    component: DashboardComponent,
+    pathMatch: 'full',
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "update",
+    component: UpdateComponent,
+    pathMatch: 'full',
+    canActivate: [AuthGuard]
+  }
+  //
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,55 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input'
+import {FormsModule} from '@angular/forms'
+import {HttpClientModule, HTTP_INTERCEPTORS} from "@angular/common/http"
+import { LoginService } from './services/login.service';
+import { AuthGuard } from './services/auth.guard';
+import { RegisterComponent } from './components/register/register.component';
+import { UpdateComponent } from './components/update/update.component';
+import { AuthInterceptor } from './services/auth.interceptor';
+import {ReactiveFormsModule} from '@angular/forms';
+import { RegisterService } from './services/register.service';
+import {MatMenuModule} from '@angular/material/menu';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent,
+    DashboardComponent,
+    RegisterComponent,
+    UpdateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSnackBarModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatMenuModule
   ],
-  providers: [],
+providers: [LoginService, AuthGuard, RegisterService ,[{provide:HTTP_INTERCEPTORS, useClass:AuthInterceptor, multi:true}] ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
